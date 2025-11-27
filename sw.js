e4ba910ce93ac56e401b8736b23e6c95e8e230a2
Anuh4x <anuhasindeewara7@gmail.com>
2025-11-27 14:12:02 +0530

    Made downloadable

---
+++ sw.js
@@ -0,0 +1,34 @@
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('inventory-cache-v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== 'inventory-cache-v1')
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
